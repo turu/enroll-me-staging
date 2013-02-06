8bc8eac5dd65f1a5cfcd34f1ac28eaa73f9fa1a1
partyks <partyks@gmail.com>
2013-02-06 14:39:59 +0100

    EnrollConfiguration import fixed

--- src/main/java/pl/agh/enrollme/webflow/services/ConfigurationDAO.java
+++ src/main/java/pl/agh/enrollme/webflow/services/ConfigurationDAO.java
@@ -1,6 +1,6 @@
 package pl.agh.enrollme.webflow.services;
 
-import com.sun.org.apache.xerces.internal.impl.XMLEntityManager;
+import javax.persistence.EntityManager;
 import org.springframework.stereotype.Service;
 import pl.agh.enrollme.webflow.model.EnrollConfiguration;
 
@@ -19,6 +19,7 @@ public class ConfigurationDAO implements IConfigurationDAO {
     public void addConfiguration(EnrollConfiguration configuration) {
         //TODO - em.merge, this is for a while only (to check if persist doesnt work when same PK is provided
         em.persist(configuration);
+
     }
 
     public Map<String, Integer> getConfigurationByID(Integer id) {
