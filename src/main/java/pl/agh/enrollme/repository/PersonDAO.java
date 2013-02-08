460c707991e6eecbb91596d58f7cf506cbb4a785
Rafał Cymerys <rafal.cym@gmail.com>
2013-02-08 16:54:08 +0100

    Added logging to PersonDAO

--- src/main/java/pl/agh/enrollme/repository/PersonDAO.java
+++ src/main/java/pl/agh/enrollme/repository/PersonDAO.java
@@ -7,24 +7,31 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
 
 import pl.agh.enrollme.model.Person;
 
 @Repository
 public class PersonDAO implements IPersonDAO {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(PersonDAO.class.getName());
+
 	@PersistenceContext
     EntityManager em;
     
     @Transactional
     public void addPerson(Person person) {
+        LOGGER.info("Saving person with id " + person.getId());
         em.persist(person);
     }
-	
+
     @Transactional
     public List<Person> listPeople() {
+        LOGGER.info("Retrieving list of people in database");
         CriteriaQuery<Person> c = em.getCriteriaBuilder().createQuery(Person.class);
         Root<Person> from = c.from(Person.class);
         c.orderBy(em.getCriteriaBuilder().desc(from.get("lastName")));
@@ -34,14 +41,17 @@ public class PersonDAO implements IPersonDAO {
 
     @Transactional
     public void removePerson(Integer id) {
+        LOGGER.info("Removing person with id " + id);
         Person person = em.find(Person.class, id);
         if (null != person) {
+            LOGGER.error("Person not found - " + id);
             em.remove(person);
         }
     }
 
     @Transactional
     public Person getPerson(Integer id) {
+        LOGGER.info("Retrieving person with id " + id);
         return em.find(Person.class, id);
     }
 }
