2c8bbea5591b9ae9803d482b458072b08780d645
partyks <partyks@gmail.com>
2013-02-12 02:19:40 +0100

    cleanup in some example Converter and Service (compilation error because of new Person constructor)

--- src/main/java/pl/agh/enrollme/service/PersonService.java
+++ src/main/java/pl/agh/enrollme/service/PersonService.java
@@ -1,11 +1,10 @@
 package pl.agh.enrollme.service;
 
+import org.springframework.stereotype.Service;
+import pl.agh.enrollme.model.Person;
+
 import java.util.ArrayList;
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import pl.agh.enrollme.model.Person;
 
 @Service
 public class PersonService {
@@ -13,11 +12,11 @@ public class PersonService {
 	private static List<Person> cache = new ArrayList<Person>();
 	
 	static {
-		cache.add(new Person(0, "Jamie", "Carr"));
-		cache.add(new Person(1, "Jean", "Cobbs"));
-		cache.add(new Person(2, "John", "Howard"));
-		cache.add(new Person(3, "John", "Mudra"));
-		cache.add(new Person(4, "Julia", "Webber"));
+//		cache.add(new Person(0, "Jamie", "Carr"));
+//		cache.add(new Person(1, "Jean", "Cobbs"));
+//		cache.add(new Person(2, "John", "Howard"));
+//		cache.add(new Person(3, "John", "Mudra"));
+//		cache.add(new Person(4, "Julia", "Webber"));
 	}
 	
 	public List<String> suggestNames(String text) {
