31bcbd1f896bdc3b7bac1309229e7d03f792200a
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-12 02:20:32 +0100

    Moved local maven repo to improve compile times. Imported upgraded version of primefaces (with enrollSchedule component)

--- src/main/java/pl/agh/enrollme/service/EnrollUserDetailsService.java
+++ src/main/java/pl/agh/enrollme/service/EnrollUserDetailsService.java
@@ -3,10 +3,12 @@ package pl.agh.enrollme.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.WebApplicationContext;
 import pl.agh.enrollme.model.Person;
 import pl.agh.enrollme.repository.IPersonDAO;
 
