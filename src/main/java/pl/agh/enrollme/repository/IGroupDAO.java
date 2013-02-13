6aad3dcb4bf212fe734f3eb3c891e768f642ef2d
Paweł Lipski <pawel.p.lipski@gmail.com>
2013-02-13 15:44:30 +0100

    Fixup

---
+++ src/main/java/pl/agh/enrollme/repository/IGroupDAO.java
@@ -0,0 +1,21 @@
+package pl.agh.enrollme.repository;
+
+import pl.agh.enrollme.model.Group;
+import pl.agh.enrollme.model.Subject;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Paweł
+ * Date: 13.02.13
+ * Time: 11:38
+ * To change this template use File | Settings | File Templates.
+ */
+public interface IGroupDAO {
+    List<Group> getGroups(Subject subject);
+
+    void tryToAddCurrentUserToGroup(Group group);
+
+    void addGroup(Subject subject);
+}
