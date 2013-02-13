4546e43a5a5b3c64ac62455a660eaf1173810524
Paweł Lipski <pawel.p.lipski@gmail.com>
2013-02-13 10:25:49 +0100

    Added user-groups

--- src/main/java/pl/agh/enrollme/repository/SubjectDAO.java
+++ src/main/java/pl/agh/enrollme/repository/SubjectDAO.java
@@ -3,7 +3,11 @@ package pl.agh.enrollme.repository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+import pl.agh.enrollme.model.Enroll;
 import pl.agh.enrollme.model.Subject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Michal Partyka
@@ -16,5 +20,11 @@ public class SubjectDAO implements ISubjectDAO {
         //TODO: fill the subjects into databse under currentUser. (for subjects, user.addSubject())...
         System.out.println(( (UserDetails) SecurityContextHolder.getContext().getAuthentication().
                 getPrincipal()).getUsername());
+    }
+
+    @Override
+    public List<Enroll> getSubjectsWithGroups(Enroll enroll) {
+        List<Enroll> result = new ArrayList<Enroll>();
+        return result;
     }
 }
