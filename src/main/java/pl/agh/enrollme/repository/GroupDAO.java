658c2f89d622352f6d21fce1f6aef25d2aa25cbb
Paweł Lipski <pawel.p.lipski@gmail.com>
2013-02-13 13:40:47 +0100

    Fixed addGroup

--- src/main/java/pl/agh/enrollme/repository/GroupDAO.java
+++ src/main/java/pl/agh/enrollme/repository/GroupDAO.java
@@ -9,6 +9,9 @@ import pl.agh.enrollme.model.*;
 import pl.agh.enrollme.utils.Color;
 import pl.agh.enrollme.utils.DayOfWeek;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,9 +24,21 @@ public class GroupDAO implements IGroupDAO {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GroupDAO.class.getName());
 
+    @PersistenceContext
+    EntityManager em;
+
     @Override
     public void addGroup(Subject subject) {
-        LOGGER.error(subject.getName() + "\n" + "\n");
+        // LOGGER.error(subject.getName() + "\n" + "\n");
+        List<Person> people = new ArrayList<Person>();
+        Person guy1 = new Person();
+        guy1.setFirstName("Edward");
+        people.add(guy1);
+        Person guy2 = new Person();
+        guy2.setFirstName("Edmund");
+        people.add(guy2);
+
+        em.persist(new Group(people, subject));
     }
 
     @Override
@@ -58,6 +73,5 @@ public class GroupDAO implements IGroupDAO {
         //System.out.println(group.getPersons().get(0).getFirstName());
         LOGGER.error(group.getPersons().get(0).getFirstName() + "\n" + "\n");
     }
-
 
 }
