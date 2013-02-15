6328bbafe182a20598a0b6f069ff91410dfd843d
Paweł Lipski <pawel.p.lipski@gmail.com>
2013-02-15 14:31:16 +0100

    Enrollment ID fixup #9

--- src/main/java/pl/agh/enrollme/repository/SubjectDAO.java
+++ src/main/java/pl/agh/enrollme/repository/SubjectDAO.java
@@ -70,10 +70,10 @@ public class SubjectDAO extends GenericDAO<Subject> implements ISubjectDAO {
         subjects.add(subject2);
         //em.merge(enroll);
         //em.persist(enroll);
-        em.merge(subject1);
-        //em.persist(subject1);
-        em.merge(subject2);
-        //em.persist(subject2);
+        //em.merge(subject1);
+        em.persist(subject1);
+        //em.merge(subject2);
+        em.persist(subject2);
         return subjects;
     }
 }
