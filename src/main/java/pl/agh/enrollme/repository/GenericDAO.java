08dd07ee2bc85879896097135e76d639b3684787
partyks <partyks@gmail.com>
2013-02-15 17:00:18 +0100

    addSubject addTeacher GenericDAO GenericService

--- src/main/java/pl/agh/enrollme/repository/GenericDAO.java
+++ src/main/java/pl/agh/enrollme/repository/GenericDAO.java
@@ -74,6 +74,7 @@ public class GenericDAO<T> implements IGenericDAO<T> {
     @Transactional
     @Override
     public T getByPK(Object PK) {
+        System.out.println("[partyks DEBUG] I have returned some object by PK");
         return em.find(type, PK);
     }
 
