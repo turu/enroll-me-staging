f5fca4ed61ebd372dfdf206211a1a45f9e3181fb
partyks <partyks@gmail.com>
2013-02-16 01:53:50 +0100

    addSubject addTeacher GenericDAO GenericService

--- src/main/java/pl/agh/enrollme/converter/TeacherConverter.java
+++ src/main/java/pl/agh/enrollme/converter/TeacherConverter.java
@@ -1,41 +1,28 @@
 package pl.agh.enrollme.converter;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 import pl.agh.enrollme.model.Teacher;
 import pl.agh.enrollme.repository.ITeacherDAO;
 
-import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 //@FacesConverter(value="teacherConverter")
-@Repository
+@Service
+@ViewScoped
 public class TeacherConverter implements Converter {
 
     @Autowired
     ITeacherDAO teacherDAO;
 
-    @PersistenceContext
-    EntityManager em;
-
 	public Object getAsObject(FacesContext context, UIComponent component, String value) {
         System.out.println("[partyks DEBUG] Get for parsing:" + value);
         Integer ID = Integer.parseInt(value);
         System.out.println("[partyks DEBUG] after parsing:" + ID);
-        if(em == null) {
-            throw new UnsupportedOperationException("KUPA3");
-        }
-        if(teacherDAO == null) {
-            throw new UnsupportedOperationException("KUPA");
-        }
-        Teacher aaa = teacherDAO.getByPK(117);
-
-//        return teacherDAO.getByPK(ID);
-        return teacherDAO.getByPK(117);
+        return teacherDAO.getByPK(ID);
 	}
 
 	public String getAsString(FacesContext context, UIComponent component, Object value) {
