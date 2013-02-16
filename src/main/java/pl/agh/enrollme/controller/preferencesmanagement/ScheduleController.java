eea2b74e848eaeeebb27313c5c4d7e4f0b0aca36
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-17 00:06:04 +0100

    Initial clean-up in ScheduleController

--- src/main/java/pl/agh/enrollme/controller/preferencesmanagement/ScheduleController.java
+++ src/main/java/pl/agh/enrollme/controller/preferencesmanagement/ScheduleController.java
@@ -16,17 +16,35 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+/**
+ * This class is used as a schedule controller in preferences-management view
+ * Author: Piotr Turek
+ */
 public class ScheduleController implements Serializable {
 
-    private static final long serialVersionUID = 1;
+    private static final long serialVersionUID = -740843017652008075L;
 
-    private EnrollScheduleModel eventModel;
+    private EnrollScheduleModel eventModel = new DefaultEnrollScheduleModel();
 
     private EnrollScheduleEvent event = new DefaultEnrollScheduleEvent();
 
     private String theme;
 
-    public ScheduleController(EnrollConfiguration enrollConfiguration, List<Subject> subjects, List<Term> terms, List<StudentPointsPerTerm> points) {
+    private EnrollConfiguration enrollConfiguration;
+
+    private List<Subject> subjects;
+
+    private List<Term> terms;
+
+    private List<StudentPointsPerTerm> points;
+
+
+    public ScheduleController(EnrollConfiguration enrollConfiguration, List<Subject> subjects, List<Term> terms,
+                              List<StudentPointsPerTerm> points) {
+        this.enrollConfiguration = enrollConfiguration;
+        this.subjects = subjects;
+        this.terms = terms;
+        this.points = points;
     }
 
     public String getTheme() {
@@ -53,71 +71,43 @@ public class ScheduleController implements Serializable {
         this.event = event;
     }
 
-    public ScheduleController() {
-        eventModel = new DefaultEnrollScheduleModel();
-        GregorianCalendar gc = new GregorianCalendar(2013, 1, 11, 10, 15);
-
-        Date begin = gc.getTime();
-        gc.add(Calendar.MINUTE, 90);
-        Date end = gc.getTime();
-        DefaultEnrollScheduleEvent newEvent = new DefaultEnrollScheduleEvent("Analiza", begin, end);
-        newEvent.setEditable(false);
-        newEvent.setTeacher("dr W. Frydrych");
-        newEvent.setPlace("s. 3.27");
-        eventModel.addEvent(newEvent);
-        DefaultEnrollScheduleEvent newEvent2 = new DefaultEnrollScheduleEvent("MOwNiT", begin, end);
-        newEvent2.setEditable(false);
-        newEvent2.setPossible(false);
-        newEvent2.setTeacher("dr W. Czech");
-        newEvent2.setPlace("s. 3.23");
-        eventModel.addEvent(newEvent2);
-        DefaultEnrollScheduleEvent newEvent3 = new DefaultEnrollScheduleEvent("PSI", begin, end);
-        newEvent3.setEditable(false);
-        newEvent3.setTeacher("dr M. Żabińska");
-        newEvent3.setPlace("2.31");
-        newEvent3.setPoints(7);
-        newEvent3.setImportance(70);
-        eventModel.addEvent(newEvent3);
-    }
-
-    public void addEvent(ActionEvent actionEvent) {
-        if(event.getId() == null)
-            eventModel.addEvent(event);
-        else
-            eventModel.updateEvent(event);
-
-        event = new DefaultEnrollScheduleEvent();
-    }
-
     public void onEventSelect(SelectEvent selectEvent) {
         event = (EnrollScheduleEvent) selectEvent.getObject();
-    }
-
-    public void onDateSelect(SelectEvent selectEvent) {
-        final Date begin = (Date) selectEvent.getObject();
-        GregorianCalendar gc = new GregorianCalendar();
-        gc.set(begin.getYear(), begin.getMonth(), begin.getDay(), begin.getHours(), begin.getMinutes());
-        gc.add(Calendar.MINUTE, 90);
-        final Date end = gc.getTime();
-        event = new DefaultEnrollScheduleEvent("", begin, end);
-    }
-
-    public void onEventMove(EnrollScheduleEntryMoveEvent moveEvent) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event moved", "Day delta:" + moveEvent.getDayDelta() + ", Minute delta:" + moveEvent.getMinuteDelta());
-        addMessage(message);
-        final EnrollScheduleEvent scheduleEvent = moveEvent.getScheduleEvent();
-        eventModel.updateEvent(scheduleEvent);
-    }
-
-    public void onEventResize(EnrollScheduleEntryResizeEvent resizeEvent) {
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Event resized", "Day delta:" + resizeEvent.getDayDelta() + ", Minute delta:" + resizeEvent.getMinuteDelta());
-        addMessage(message);
-        final EnrollScheduleEvent scheduleEvent = resizeEvent.getScheduleEvent();
-        eventModel.updateEvent(scheduleEvent);
     }
 
     private void addMessage(FacesMessage message) {
         FacesContext.getCurrentInstance().addMessage(null, message);
     }
 
+    public EnrollConfiguration getEnrollConfiguration() {
+        return enrollConfiguration;
+    }
+
+    public void setEnrollConfiguration(EnrollConfiguration enrollConfiguration) {
+        this.enrollConfiguration = enrollConfiguration;
+    }
+
+    public List<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(List<Subject> subjects) {
+        this.subjects = subjects;
+    }
+
+    public List<Term> getTerms() {
+        return terms;
+    }
+
+    public void setTerms(List<Term> terms) {
+        this.terms = terms;
+    }
+
+    public List<StudentPointsPerTerm> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<StudentPointsPerTerm> points) {
+        this.points = points;
+    }
 }
