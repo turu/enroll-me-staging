dcb50ded23aa5566853fc545594ed0faf713de6f
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-21 13:33:53 +0100

    Fixed a compilation issue

--- src/main/java/pl/agh/enrollme/service/SubjectChoosingService.java
+++ src/main/java/pl/agh/enrollme/service/SubjectChoosingService.java
@@ -23,19 +23,19 @@ public class SubjectChoosingService implements ISubjectChoosingService {
     @Override
     public List<Subject> getAvailableSubjectForEnrollment(Enroll enroll) {
         //TODO: later get from database, enroll.getSubjects() ;-)
-        Teacher teacher1 = new Teacher("dr", "Stanisław", "Sobieszko", "4.11");
-        Teacher teacher2 = new Teacher("dr", "Stasio", "Mieszko", "4.11");
-        Subject subject1 = new Subject(enroll, null, "Analiza", 1, "#00ffff", "4.33", teacher1, DayOfWeek.MONDAY,
-                null, null);
-        Subject subject2 = new Subject(enroll, null, "Fizyka", 1, "#ff0000", "4.11", teacher2, DayOfWeek.FRIDAY,
-                null, null);
-        subject1.setSubjectID(1);
-        subject2.setSubjectID(2);
-        List<Subject> subjects = new ArrayList<Subject>(2);
-        subjects.add(subject1);
-        subjects.add(subject2);
-        model = new SelectableDataModelForSubjects(subjects);
-        return subjects;
+//        Teacher teacher1 = new Teacher("dr", "Stanisław", "Sobieszko", "4.11");
+//        Teacher teacher2 = new Teacher("dr", "Stasio", "Mieszko", "4.11");
+//        Subject subject1 = new Subject(enroll, null, "Analiza", 1, "#00ffff", "4.33", teacher1, DayOfWeek.MONDAY,
+//                null, null);
+//        Subject subject2 = new Subject(enroll, null, "Fizyka", 1, "#ff0000", "4.11", teacher2, DayOfWeek.FRIDAY,
+//                null, null);
+//        subject1.setSubjectID(1);
+//        subject2.setSubjectID(2);
+//        List<Subject> subjects = new ArrayList<Subject>(2);
+//        subjects.add(subject1);
+//        subjects.add(subject2);
+//        model = new SelectableDataModelForSubjects(subjects);
+        return null;
     }
 
     public boolean userAlreadySubmitedSubjects() {
