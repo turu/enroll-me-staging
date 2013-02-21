d8e52b2b0d55c9de90b9772640087bdc4e3a62cd
partyks <partyks@gmail.com>
2013-02-21 03:05:19 +0100

    some minor fix

--- src/main/java/pl/agh/enrollme/repository/EnrollmentDAO.java
+++ src/main/java/pl/agh/enrollme/repository/EnrollmentDAO.java
@@ -2,17 +2,7 @@ package pl.agh.enrollme.repository;
 
 import org.hibernate.Hibernate;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 import pl.agh.enrollme.model.Enroll;
-import pl.agh.enrollme.model.EnrollConfiguration;
-import pl.agh.enrollme.utils.EnrollmentMode;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * @author Michal Partyka
@@ -28,11 +18,5 @@ public class EnrollmentDAO extends GenericDAO<Enroll> implements IEnrollmentDAO 
         Enroll enrollment = super.getByPK(PK);
         Hibernate.initialize(enrollment.getSubjects());
         return enrollment;
-    }
-    
-    //Onlyfor debug TODO: remove later
-    public void testByID(Integer id) {
-        Enroll enroll = getEntityManager().find(Enroll.class, id);
-        enroll.getSubjects();
     }
 }
