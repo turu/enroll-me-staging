35bf0f7014e6cdda2ce9a8fe70b7766458b69c84
partyks <partyks@gmail.com>
2013-02-21 17:33:40 +0100

    controller scope fix

--- src/main/java/pl/agh/enrollme/controller/TestScheduleController.java
+++ src/main/java/pl/agh/enrollme/controller/TestScheduleController.java
@@ -12,7 +12,7 @@ import pl.agh.enrollme.model.Term;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionEvent    ;
 import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
