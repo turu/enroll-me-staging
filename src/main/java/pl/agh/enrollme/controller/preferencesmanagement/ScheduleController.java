6018ba8a8e808d8ebfcdaf64964e3bf9284ee00d
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-24 23:29:46 +0100

    Some obvious fixes plus logging added to ScheduleController

--- src/main/java/pl/agh/enrollme/controller/preferencesmanagement/ScheduleController.java
+++ src/main/java/pl/agh/enrollme/controller/preferencesmanagement/ScheduleController.java
@@ -136,65 +136,107 @@ public class ScheduleController implements Serializable {
      * @param selectEvent
      */
     public void onEventSelect(SelectEvent selectEvent) {
+        LOGGER.debug("Event Select triggered");
         event = (DefaultEnrollScheduleEvent) selectEvent.getObject();
 
-        final Term term = eventToTermMap.get(event);
+        final Term term = eventToTermMap.get(event.getId());
+        LOGGER.debug("Event: " + event + " clicked, corresponding term: " + term + " retrieved");
+
         final StudentPointsPerTerm termPoints = termToPointsMap.get(term);
+        LOGGER.debug("Points: " + termPoints + " retrieved");
+
         final Subject subject = term.getSubject();
+        LOGGER.debug("Subject: " + subject + " retrieved");
+
         final Map<Integer, Integer> pointsMap = progressController.getPointsMap();
 
         reason = termPoints.getReason();
-        eventPointsRange = enrollConfiguration.getPointsPerSubject() - pointsMap.get(subject.getSubjectID())
-                + termPoints.getPoints();
-        eventPointsRange = Math.min(eventPointsRange, enrollConfiguration.getPointsPerTerm());
+
+        if (!event.isPossible()) {
+            eventPointsRange = 0;
+        } else {
+            eventPointsRange = enrollConfiguration.getPointsPerSubject() - pointsMap.get(subject.getSubjectID())
+                    + termPoints.getPoints();
+            eventPointsRange = Math.min(eventPointsRange, enrollConfiguration.getPointsPerTerm());
+        }
+
+        LOGGER.debug("Points range computed to be: " + eventPointsRange);
     }
 
     /**
      * Updates current event (kept in event field)
      */
-    public boolean updateEvent(ActionEvent actionEvent) {
-        final Term term = eventToTermMap.get(event);
+    public void updateEvent(ActionEvent actionEvent) {
+        LOGGER.debug("Event Update triggered");
+
+        final Term term = eventToTermMap.get(event.getId());
+        LOGGER.debug("Event: " + event + " clicked, corresponding term: " + term + " retrieved");
+
         final StudentPointsPerTerm termPoints = termToPointsMap.get(term);
+        LOGGER.debug("Points: " + termPoints + " retrieved");
+
         final int extraPointsLeft = enrollConfiguration.getAdditionalPoints() - progressController.getExtraPointsUsed();
+        LOGGER.debug("ExtraPointsLeft: " + extraPointsLeft);
+
         final int pointsDelta = event.getPoints() - termPoints.getPoints();
+        LOGGER.debug("Points delta: " + pointsDelta);
 
         if (!event.isPossible()) {
             final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Choice Changed",
                     "You've just set an impossibility. It will be reviewed by a year representative." +
                             " Remember to save your changes frequently!!!");
             addMessage(message);
+
             termPoints.setReason(reason);
             termPoints.setPoints(-1);
+            LOGGER.debug("Impossibility set, reason: " + reason);
+
             progressController.update();
+            LOGGER.debug("Progress update called");
+
             event.setShowPoints(false);
+
             eventModel.updateEvent(event);
-            return true;
+
+            return;
         }
 
-        if (termPoints.getPoints() > enrollConfiguration.getPointsPerTerm()) {
+        //Enforcing base boundaries on term points
+        if (event.getPoints() > enrollConfiguration.getPointsPerTerm() || event.getPoints() < -1) {
             final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Rule Broken!",
                     "You surpassed limit of points per term. Change rejected.");
             addMessage(message);
-            return false;
+
+            LOGGER.debug("Base boundaries rules broken!");
+
+            return;
         }
 
+
+        //Enforcing (upper) boundaries on subject points
         if (termPoints.getPoints() + pointsDelta > enrollConfiguration.getPointsPerSubject() + extraPointsLeft) {
             final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_WARN, "Rule Broken!",
                     "You surpassed limit of points per subject. Change rejected.");
             addMessage(message);
-            return false;
+
+            LOGGER.debug("Subject points rules broken");
+
+            return;
         }
 
-        termPoints.setPoints(termPoints.getPoints() + pointsDelta);
+        termPoints.setPoints(event.getPoints());
+        LOGGER.debug("New points for term: " + term + " set to: " + termPoints.getPoints());
+
         event.setShowPoints(true);
-        setEventImportance(event);
         event.setPossible(true);
+
         final FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Choice Changed",
                 "You've just altered your choice (points delta: " + pointsDelta + "). Currently there are " +
                         event.getPoints() + " points assigned to this term. Remember to save your changes frequently!");
 
         eventModel.updateEvent(event);
-        return true;
+
+        LOGGER.debug("Event updated");
     }
 
 
@@ -292,7 +334,9 @@ public class ScheduleController implements Serializable {
         //create mapping: Term -> StudentPointsPerTerm to allow for efficient access to data
         for (StudentPointsPerTerm p : points) {
             termToPointsMap.put(p.getTerm(), p);
+            LOGGER.debug("Creating map entry for term: " + p.getTerm() + " and points: " + p);
         }
+        LOGGER.debug(termToPointsMap.size() + " entries created");
 
         int minHour = Integer.MAX_VALUE;
         int maxHour = Integer.MIN_VALUE;
@@ -303,6 +347,7 @@ public class ScheduleController implements Serializable {
 
         //preprocess terms, computing scope of enrollment, creating events etc.
         for (Term t : terms) {
+            LOGGER.debug("Processing of term: " + t);
             GregorianCalendar startTime = new GregorianCalendar();
             startTime.setTime(t.getStartTime());
             GregorianCalendar endTime = new GregorianCalendar();
@@ -311,6 +356,7 @@ public class ScheduleController implements Serializable {
             //if term starts on a Saturday or Sunday, set showWeekends to true
             if(startTime.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || startTime.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
                 showWeekends = true;
+                LOGGER.debug("ShowWeekends set to true");
             }
 
             final int termStartHour = startTime.get(Calendar.HOUR_OF_DAY);
@@ -333,11 +379,13 @@ public class ScheduleController implements Serializable {
             }
 
             StudentPointsPerTerm p = termToPointsMap.get(t);
+            LOGGER.debug("Points: " + p + " retrieved");
             DefaultEnrollScheduleEvent event = new DefaultEnrollScheduleEvent();
 
             //setting event's points
             if (p.getPoints() == -1) {
                 event.setPoints(0);
+                event.setPossible(false);
             } else {
                 event.setPoints(p.getPoints());
             }
@@ -356,8 +404,11 @@ public class ScheduleController implements Serializable {
             //setting event's title to subjects' name
             event.setTitle(subject.getName());
 
-            //setting event possibility
-            event.setPossible(p.getPoints() != -1);
+            //setting event's start date
+            event.setStartDate(t.getStartTime());
+
+            //setting event's end date
+            event.setEndDate(t.getEndTime());
 
             //setting event importance as percent of total points available to this event
             setEventImportance(event);
@@ -369,19 +420,22 @@ public class ScheduleController implements Serializable {
             event.setActivityType(t.getType());
 
             //setting whether event is static or not
-            event.setInteractive(!t.getCertain());
+            event.setInteractive(!t.getCertain() && !p.getAssigned());
 
             //setting whether to display points or not
-            event.setShowPoints(!t.getCertain() && event.isPossible());
+            event.setShowPoints(!t.getCertain() && event.isPossible() && !p.getAssigned());
 
             //event's shouldn't be editable
             event.setEditable(false);
 
             //adding event to the model
             eventModel.addEvent(event);
+            LOGGER.debug("New event: " + event + " added to eventModel");
 
             eventToTermMap.put(event.getId(), t);
+            LOGGER.debug("Term processing finished");
         }
+        LOGGER.debug("All terms processed");
 
         //update time fields, but only if there were some events added, otherwise use defaults
         if (terms.size() > 0) {
@@ -404,7 +458,11 @@ public class ScheduleController implements Serializable {
     }
 
     private void setEventImportance(DefaultEnrollScheduleEvent event) {
-        event.setImportance((int) ((double) event.getPoints() / (double) enrollConfiguration.getPointsPerTerm() * 100));
+        if (event.isPossible()) {
+            event.setImportance((int) ((double) event.getPoints() / (double) enrollConfiguration.getPointsPerTerm() * 100));
+        } else {
+            event.setImportance(100);
+        }
     }
 
 }
