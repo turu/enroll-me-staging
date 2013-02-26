18c99e6a3ac20be67e075e7180496b3487aa4013
Paweł Lipski <pawel.p.lipski@gmail.com>
2013-02-26 10:32:17 +0100

    Result view

--- src/main/java/pl/agh/enrollme/repository/TeacherDAO.java
+++ src/main/java/pl/agh/enrollme/repository/TeacherDAO.java
@@ -1,14 +1,36 @@
 package pl.agh.enrollme.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import pl.agh.enrollme.model.Subject;
 import pl.agh.enrollme.model.Teacher;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 /**
  * @author Michal Partyka
  */
 @Repository
 public class TeacherDAO extends GenericDAO<Teacher> implements ITeacherDAO {
+
+    @PersistenceContext
+    EntityManager em;
+
     public TeacherDAO() {
         super(Teacher.class);
+    }
+
+    @Override
+    @Transactional
+    public List<Teacher> getTeachersForSubject(Subject subject) {
+        final String queryString = "Select distinct teacher from Term t where t.subject = :subject";
+        final TypedQuery<Teacher> query = em
+                .createQuery(queryString, Teacher.class)
+                .setParameter("subject", subject);
+
+        return query.getResultList();
     }
 }
