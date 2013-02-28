7af801bb8ccb21cb4c04354ed4f6bf97d93e3a86
Piotr Turek <piotr.turu.turek@gmail.com>
2013-02-28 14:31:01 +0100

    Reset service fix zilion 2

--- src/main/java/pl/agh/enrollme/service/PassResetService.java
+++ src/main/java/pl/agh/enrollme/service/PassResetService.java
@@ -49,7 +49,7 @@ public class PassResetService implements IPassResetService {
 
         personService.setEncodedPassword(person, controller.getNewPass());
 
-        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Password change", "New password has been changed.");
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Password change", "Your password has been changed.");
         addMessage(message);
 
     }
